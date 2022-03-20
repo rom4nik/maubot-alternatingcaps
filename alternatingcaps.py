154f73a53fb39ac30dcc7874365d2d1ab553e8f5
rom4nik <46846000+rom4nik@users.noreply.github.com>
2022-03-20 02:28:33 +0100

    remember last message per room

--- alternatingcaps.py
+++ alternatingcaps.py
@@ -4,13 +4,13 @@ from mautrix.types import EventType
 
 
 class AlternatingCaps(Plugin):
-    last_message = None
+    last_messages = {}
 
     @event.on(EventType.ROOM_MESSAGE)
     async def message_handler(self, evt: MessageEvent) -> None:
-        self.last_message = evt.content.body
+        self.last_messages[evt.room_id] = evt.content.body
 
     @command.new()
     async def altcaps(self, evt: MessageEvent) -> None:
-        if self.last_message:
-            await evt.respond("".join([c.upper() if i % 2 else c.lower() for i, c in enumerate(self.last_message)]))
+        if evt.room_id in self.last_messages:
+            await evt.respond("".join([c.upper() if i % 2 else c.lower() for i, c in enumerate(self.last_messages[evt.room_id])]))
