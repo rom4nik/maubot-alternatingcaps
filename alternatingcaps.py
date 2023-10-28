c2a91cad7e623852ffe1c6d13817fb8368012be7
Przemysław Romanik <46846000+rom4nik@users.noreply.github.com>
2023-10-28 05:17:54 +0200

    Forget last message when it gets redacted

--- alternatingcaps.py
+++ alternatingcaps.py
@@ -1,6 +1,6 @@
-from maubot import Plugin, MessageEvent
+from maubot import Plugin
 from maubot.handlers import command, event
-from mautrix.types import EventType
+from mautrix.types import EventType, MessageEvent, RedactionEvent
 
 
 class AlternatingCaps(Plugin):
@@ -8,9 +8,18 @@ class AlternatingCaps(Plugin):
 
     @event.on(EventType.ROOM_MESSAGE)
     async def message_handler(self, evt: MessageEvent) -> None:
-        self.last_messages[evt.room_id] = evt.content.body
+        if evt.sender != self.client.mxid:
+            self.last_messages[evt.room_id] = {
+                "id": evt.event_id,
+                "body": evt.content.body
+            }
+
+    @event.on(EventType.ROOM_REDACTION)
+    async def redaction_handler(self, evt: RedactionEvent) -> None:
+        if evt.room_id in self.last_messages and evt.redacts == self.last_messages[evt.room_id]["id"]:
+                self.last_messages.pop(evt.room_id)
 
     @command.new()
     async def altcaps(self, evt: MessageEvent) -> None:
         if evt.room_id in self.last_messages:
-            await evt.respond("".join([c.upper() if i % 2 else c.lower() for i, c in enumerate(self.last_messages[evt.room_id])]))
+            await evt.respond("".join([c.upper() if i % 2 else c.lower() for i, c in enumerate(self.last_messages[evt.room_id]["body"])]))
