516b3d40b77500da9ebe07f5771a6c8e1fa16922
William Kray <williamkray@users.noreply.github.com>
2024-11-11 13:20:10 -0800

    feat: allow altcap behavior for a specific message if replied to, add `spongebob`, `alternatingcaps` aliases (#3)

--- alternatingcaps.py
+++ alternatingcaps.py
@@ -6,6 +6,9 @@ from mautrix.types import EventType, MessageEvent, RedactionEvent
 class AlternatingCaps(Plugin):
     last_messages = {}
 
+    def is_alias(self, command: str) -> bool:
+        return command == 'altcaps' or command in ["spongebob", "alternatingcaps"]
+
     @event.on(EventType.ROOM_MESSAGE)
     async def message_handler(self, evt: MessageEvent) -> None:
         if evt.sender != self.client.mxid:
@@ -19,7 +22,12 @@ class AlternatingCaps(Plugin):
         if evt.room_id in self.last_messages and evt.redacts == self.last_messages[evt.room_id]["id"]:
                 self.last_messages.pop(evt.room_id)
 
-    @command.new()
+    @command.new(aliases=is_alias)
     async def altcaps(self, evt: MessageEvent) -> None:
-        if evt.room_id in self.last_messages:
-            await evt.respond("".join([c.upper() if i % 2 else c.lower() for i, c in enumerate(self.last_messages[evt.room_id]["body"])]))
+        if evt.content.get_reply_to():
+            target = await self.client.get_event(evt.room_id, evt.content.get_reply_to())
+            body = target.content.body
+        elif evt.room_id in self.last_messages:
+            body = self.last_messages[evt.room_id]["body"]
+
+        await evt.respond("".join([c.upper() if i % 2 else c.lower() for i, c in enumerate(body)]))
